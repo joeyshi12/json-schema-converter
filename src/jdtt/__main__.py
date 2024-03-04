d86253045be6d9c59f44ac98b81ed1c9d21e7a64
Joey Shi <joeyshi12@gmail.com>
2024-03-04 03:04:34 -0800

    Refactor code to make it more testable (#3)
    * initial commit
    
    * improve cli
    
    * update readme

--- src/jdtt/__main__.py
+++ src/jdtt/__main__.py
@@ -1,27 +1,32 @@
+import sys
 import json
 import argparse
-from jdtt.conversion import json_to_language_str, TargetLanguage, file_extensions
+from jdtt.transcompilation import transcompile
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--target_language", type=str, choices=["python", "scala", "typescript"], default="python", help="target language")
-    parser.add_argument("--root_schema", type=str, default="Root", help="name of root schema")
-    parser.add_argument("--detect_date", action="store_true", help="detect datetime fields and convert to date type")
-    parser.add_argument("schema_path", type=str, help="filepath to schema json")
+    parser.add_argument("-l", "--language", type=str, choices=["python", "scala", "typescript"], default="python", help="target language for transpilation")
+    parser.add_argument("-n", "--schema_name", type=str, default="Schema", help="name of the schema")
+    parser.add_argument("-d", "--detect_date", action="store_true", help="include to infer datetime fields")
+    parser.add_argument("json_path", type=str, help="JSON filepath")
     args = parser.parse_args()
 
-    schema_path = args.schema_path
-    target_language = TargetLanguage[args.target_language.upper()]
-    target_path = schema_path + file_extensions[target_language]
+    json_path = args.json_path
+    target_language = args.language.lower()
+    schema_name = "Schema" if args.schema_name is None else args.schema_name
+    date_format = r"\d{4}-\d{2}-\d{2}(T\d{2}:\d{2}:\d{2})?(\.\d{3})?Z" if args.detect_date else None
 
-    with open(schema_path, "r", encoding="utf-8") as f:
-        schema_json = json.load(f)
+    with open(json_path, "r", encoding="utf-8") as f:
+        try:
+            schema_json = json.load(f)
+            result = transcompile(schema_json, target_language, date_format, schema_name)
+            sys.stdout.write(result)
+            return
+        except Exception as e:
+            pass
 
-    with open(target_path, "w", encoding="utf-8") as f:
-        language_str = json_to_language_str(schema_json, target_language, args.root_schema)
-        f.write(language_str)
-        print(f"{args.target_language} schemas written to {target_path}")
+    sys.stderr.write("Invalid JSON file: " + json_path)
 
 
 if __name__ == "__main__":
